50332b7cc4d9688322a9955815d4f24d545476fc
Leonel Garcia Jr <leo2nel56@gmail.com>
2019-06-23 14:25:56 -0500

    Not yet fixed.

--- texasfuelratepredictor/config.py
+++ texasfuelratepredictor/config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    SECRET_KEY = 'b06af82bdbb223bb5f91a4300f173437'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     # for use with email verification
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
