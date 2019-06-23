0522b02faf85ff7b96260642052df9a79191e537
Leonel Garcia Jr <leo2nel56@gmail.com>
2019-06-23 00:29:21 -0500

    Final Structre; not done

--- texasfuelratepredictor/config.py
+++ texasfuelratepredictor/config.py
@@ -5,3 +5,9 @@ class Config:
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
+	# for use with email verification
+	MAIL_SERVER = 'smtp.googlemail.com'
+	MAIL_PORT = 587
+	MAIL_USE_TLS = True
+	MAIL_USERNAME = os.environ.get('EMAIL_USER')
+	MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
